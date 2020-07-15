a33a4dc9871bd832c53369e36444f66059a31745
Mary Dick <maryclairedick@gmail.com>
2020-07-15 22:08:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board([])
-  
+def display_board(move = [])
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
